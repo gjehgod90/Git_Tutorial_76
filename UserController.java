1147a419b3907c853fb00f8a864030857cd829bb
kosmo15 <gjehgod90@gmail.com>
2021-03-16 16:55:50 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -4,16 +4,23 @@ public class UserController {
 	// 로그인 페이지로 이동
 	@RequestMapping("/user/login.do")
 	public String login() {
-		logger.info("경로 : login");
+		logger.info("login : login");
 
 		return "user/login";
 	}
 
 	@RequestMapping("/user/insert.do")
 	public String insert() {
-		logger.info("경로 : insert");
+		logger.info("insert : insert");
 
 		return "user/insert";
+	}
+
+	@RequestMapping("/user/update.do")
+	public String update() {
+		logger.info("update : update");
+
+		return "user/update";
 	}
 }
 
