43778e087b375d6ee259af732880a073a7b9aa0d
kosmo15 <gjehgod90@gmail.com>
2021-03-16 16:11:14 +0900

    Add UserController [Insert]

--- UserController.java
+++ UserController.java
@@ -8,4 +8,11 @@ public class UserController {
 
 		return "user/login";
 	}
+
+	@RequestMapping("/user/insert.do")
+	public String insert() {
+		logger.info("경로 : insert");
+
+		return "user/insert";
+	}
 }
